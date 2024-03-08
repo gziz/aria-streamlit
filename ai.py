1f0724e380c42a254d0a11c42b3493321a016867
gziz <69287011+gziz@users.noreply.github.com>
2024-03-08 16:19:14 -0600

    Fix BASE_URL

--- ai.py
+++ ai.py
@@ -2,10 +2,10 @@
 import os
 import requests
 
-URL = os.getenv("ARIA_API_URL")
+API_URL = os.getenv("ARIA_API_URL")
 def send_question(text: str):
     payload = {"message": {"text": text}}
-    print(payload)
-    response = requests.post(URL, json=payload)
+    QUESTION_URL = API_URL + "/question"
+    response = requests.post(QUESTION_URL, json=payload)
     res_json = response.json()
     return res_json["answer"]
